5350755f75ef58917d46ad614d142831760ccb1f
박윤환 <park-yunhwan@bag-yunhwan-ui-MacBookPro.local>
2023-09-19 15:52:14 +0900

    feat: list 페이지 dynamic rendering 설정

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -3,6 +3,9 @@ import { connectDB } from "../../../util/database";
 import Link from "next/link";
 import ListItem from "./ListItem";
 
+//dynamic rendering
+export const dynamic = "force-dynamic";
+//export const dynamic = 'force-static'
 async function List() {
   // 축약 가능
   const db = (await connectDB).db("forum");
